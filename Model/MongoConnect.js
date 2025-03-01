fd66d2e156c012fc90a2371b9b041e5e8d584920
Byzid Bostami <bostami5g@gmail.com>
2025-03-02 01:51:47 +0600

    second commit

--- Model/MongoConnect.js
+++ Model/MongoConnect.js
@@ -1,15 +1,22 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 
-const mongoConnect = async()=>{
-    try {
-        const Url = process.env.MONGODB_CONNECT_URL;
-        await  mongoose.connect(Url);
-        console.log("mongo is connected succesfully");
-   
-    } catch (error) {
-        console.log(error.message);
+const mongoConnect = async () => {
+  try {
+    const url = process.env.MONGODB_CONNECT_URL;
+    if (!url) {
+      throw new Error('MONGODB_CONNECT_URL is not defined in the environment variables');
     }
-}
+    await mongoose.connect(url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 20000,
+    });
+    console.log("Mongo is connected successfully");
+  } catch (error) {
+    console.error("Mongo connection error:", error.message);
+    process.exit(1);
+  }
+};
 
-module.exports = {mongoConnect};
+module.exports = { mongoConnect };
